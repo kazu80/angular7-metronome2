4d855df9447ef8e0c1e6305c070ac46fca49ad9b
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-12-16 10:19:59 +0900

    build - opening - pc

--- src/app/opening/pc/pc.component.ts
+++ src/app/opening/pc/pc.component.ts
@@ -3,34 +3,56 @@ import {Component, ElementRef, OnInit} from '@angular/core';
 declare var PIXI: any;
 
 @Component({
-  selector: 'app-opening-pc',
+  selector   : 'app-opening-pc',
   templateUrl: './pc.component.html',
-  styleUrls: ['./pc.component.scss']
+  styleUrls  : ['./pc.component.scss']
 })
 export class PcComponent implements OnInit {
   public app: any;
+  public texts: Array = [];
 
   private _el: HTMLElement;
 
   constructor(el: ElementRef) {
-      this._el = el.nativeElement;
+    this._el = el.nativeElement;
 
-      // Pixi.js
-      this.app = new PIXI.Application({
-          width: 800,
-          height: 600
-      });
+    // Pixi.js
+    this.app = new PIXI.Application({});
   }
 
   ngOnInit() {
-      this.app.renderer.view.style.position = 'absolute';
-      this.app.renderer.view.style.display = 'block';
-      this.app.renderer.autoResize = true;
-      this.app.renderer.resize(window.innerWidth, window.innerHeight);
-      this.app.renderer.backgroundColor = 0xffffff;
+    this.app.renderer.view.style.position = 'absolute';
+    this.app.renderer.view.style.display  = 'block';
+    this.app.renderer.autoResize          = true;
+    this.app.renderer.resize(window.innerWidth, window.innerHeight);
+    this.app.renderer.backgroundColor = 0xffffff;
 
-      // Add the canvas that Pixi automatically created for you to the HTML document
-      this._el.querySelector('#canvas-wrapper').appendChild(this.app.view);
+    // Set items on initial
+    this.setInitialItems();
+
+    // Add the canvas that Pixi automatically created for you to the HTML document
+    this._el.querySelector('#canvas-wrapper').appendChild(this.app.view);
   }
 
+  setInitialItems() {
+    /** Initial message **/
+    const button      = this.addText('METRONOME', {fontSize: '80px', fontFamily: 'brandon-grotesque', fontWeight: 'BOLD'});
+    button.anchor.x   = 0.5;
+    button.anchor.y   = 0.5;
+    button.position.x = window.innerWidth * 0.5;
+    button.position.y = window.innerHeight * 0.5;
+
+    // ボタンをステージに追加
+    this.app.stage.addChild(button);
+  }
+
+  /**
+   * Add Texts
+   */
+  addText(string, style) {
+    const text = new PIXI.Text(string, style);
+    this.texts.push(text);
+
+    return text;
+  }
 }
