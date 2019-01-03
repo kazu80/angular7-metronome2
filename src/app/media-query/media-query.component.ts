8833fa61db1fffb82cd3e9c09841b293111714dc
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-01-04 07:33:35 +0900

    media-query components - Display none

--- src/app/media-query/media-query.component.ts
+++ src/app/media-query/media-query.component.ts
@@ -1,16 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output} from '@angular/core';
 
+/**
+ * media-query components
+ * Reference : https://github.com/PolymerElements/iron-media-query
+ */
 @Component({
   selector   : 'app-media-query',
   templateUrl: './media-query.component.html',
   styleUrls  : ['./media-query.component.scss']
 })
 export class MediaQueryComponent implements OnInit {
+  @Output() queryMatches: EventEmitter<any> = new EventEmitter();
 
-  constructor() {
+  private _el: HTMLElement;
+
+  constructor(el: ElementRef) {
+    this._el = el.nativeElement;
   }
 
   ngOnInit() {
+    this._el.style.display = 'none';
   }
 
 }
