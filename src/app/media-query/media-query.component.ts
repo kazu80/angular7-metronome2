fc3f488f0604514695bef63342752e981597117a
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-01-05 09:54:00 +0900

    media-query components - add queryChanged method

--- src/app/media-query/media-query.component.ts
+++ src/app/media-query/media-query.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 /**
  * media-query components
@@ -9,10 +9,13 @@ import {Component, ElementRef, EventEmitter, OnInit, Output} from '@angular/core
   templateUrl: './media-query.component.html',
   styleUrls  : ['./media-query.component.scss']
 })
-export class MediaQueryComponent implements OnInit {
+export class MediaQueryComponent implements OnInit, OnChanges {
+  @Input() query: string;
+  @Input() full: boolean;
   @Output() queryMatches: EventEmitter<any> = new EventEmitter();
 
   private _el: HTMLElement;
+  private _mq: MediaQueryList;
 
   constructor(el: ElementRef) {
     this._el = el.nativeElement;
@@ -20,6 +23,31 @@ export class MediaQueryComponent implements OnInit {
 
   ngOnInit() {
     this._el.style.display = 'none';
+    this.queryMatches.emit();
+
+    this.queryChanged();
   }
 
+  ngOnChanges(changes) {
+    if (changes.hasOwnProperty('query')) {
+      this.queryChanged();
+    }
+  }
+
+  queryChanged() {
+    // this._remove();
+    let query = this.query;
+
+    if (!query) {
+      return;
+    }
+
+    if (!this.full && query[0] !== '(') {
+      query = '(' + query + ')';
+    }
+
+    this._mq = window.matchMedia(query);
+    // this._add();
+    // this.queryHandler(this._mq);
+  }
 }
