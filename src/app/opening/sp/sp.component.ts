486fcbdbb647f442390ca62a64df55abf311603e
kawakami <kkawakami@smartshopping.co.jp>
2019-01-08 21:05:36 +0900

    Durationをつけないと表示されない

--- src/app/opening/sp/sp.component.ts
+++ src/app/opening/sp/sp.component.ts
@@ -1,6 +1,8 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {MainService} from '../../service/main.service';
-import {PixiService} from '../../service/pixi.service';
+import {PixiService, PixiText} from '../../service/pixi.service';
+
+declare var PIXI: any;
 
 @Component({
   selector   : 'app-opening-sp',
@@ -8,7 +10,11 @@ import {PixiService} from '../../service/pixi.service';
   styleUrls  : ['./sp.component.scss']
 })
 export class SpComponent implements OnInit {
+  public app: any;
+
   private _el: HTMLElement;
+
+  private _text01: PixiText;
 
   constructor(
     el: ElementRef,
@@ -29,7 +35,11 @@ export class SpComponent implements OnInit {
   eventOfMode() {
     this.mainService.modeSP.subscribe((mode: string) => {
       switch (mode) {
-        case '':
+        case 'stage0':
+          this.openingInit();
+          break;
+        case 'stage1':
+          this.stage1();
           break;
       }
     });
@@ -40,5 +50,46 @@ export class SpComponent implements OnInit {
           break;
       }
     });
+  }
+
+  openingInit() {
+    // Pixi.js
+    this.app = new PIXI.Application({});
+    this.app.renderer.view.style.position = 'absolute';
+    this.app.renderer.view.style.display = 'block';
+    this.app.renderer.autoResize = true;
+    this.app.renderer.resize(window.innerWidth, window.innerHeight);
+    this.app.renderer.backgroundColor = 0xff0000;
+
+    // Add the canvas that Pixi automatically created for you to the HTML document
+    this._el.querySelector('#canvas-wrapper-sp').appendChild(this.app.view);
+
+    this.mainService.setModeSP('stage1');
+  }
+
+  stage1() {
+    this._text01 = this.pixiService.text();
+    this._text01.value = 'START';
+
+    this._text01.style.fontSize = '36px';
+    this._text01.style.fontWeight = 'bold';
+    this._text01.style.align = 'center';
+
+    this._text01.position.x = window.innerWidth * 0.5;
+    this._text01.position.y = window.innerHeight * 0.5;
+    this._text01.anchor.x = 0.5;
+    this._text01.anchor.y = 0.5;
+
+    // this._text01.animation.alpha.from = 0;
+    // this._text01.animation.alpha.to   = 1;
+    // this._text01.animation.blur.from  = 5;
+    // this._text01.animation.blur.to    = 0;
+    this._text01.animation.duration = 500;
+    // this._text01.animation.delay = 800;
+
+    console.log('foo');
+
+    this._text01.put(this.app.stage);
+    this._text01.run('stage1', this.app.ticker);
   }
 }
